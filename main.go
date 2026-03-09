3419021c68f0c2a64656f9a8b2c1c20be1d675f5
vtrnduda <vtrnduda@gmail.com>
2026-03-09 09:05:23 -0300

    feat: init main.go structure

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var numbers []int 
+    var option int
+
+    for {
+        fmt.Println("\n--- Gerenciador de Números ---")
+        fmt.Println("1) Adicionar número")
+        fmt.Println("0) Sair")
+        fmt.Print("Escolha uma opção: ")
+        fmt.Scan(&opton)
+
+        if option == 0 {
+            break
+        }
+
+        switch opcao {
+        case 1:
+            
+        case 2:
+
+        default:
+            fmt.Println("Opção inválida!")
+        }
+    }
+}
